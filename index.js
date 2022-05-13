d686e336dd80fba154d861e4a56c76aa608eb645
TessShearer <shearer.tess@gmail.com>
2022-05-13 14:54:31 -0600

    construct employees

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const inquirer = require('inquirer');
 
+function createTeam() {
+    return inquirer.prompt(
+        {
+        type: `input`,
+        name: `name`,
+        message: `What is the employee's Name?`
+      },
+      {
+        type: `input`,
+        name: `id`,
+        message: `What is the employee's ID number?`,
+      },
+      {
+          type: `input`,
+          name: `email`,
+          message: `What is the employee's email address?`
+      }
+
+)};
+
+createTeam();
