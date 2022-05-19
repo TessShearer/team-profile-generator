3dcb2a827529135a4ed3b6f50585f9eb5f45d141
TessShearer <shearer.tess@gmail.com>
2022-05-19 10:21:41 -0600

    separate functions for adding employees

--- index.js
+++ index.js
@@ -1,41 +1,138 @@
 const inquirer = require('inquirer');
 const { writeFile, copyFile } = require('./utils/generate-site.js');
 
+
+// Creates card for first manager
 function createTeam() {
     return inquirer.prompt(
         {
         type: `input`,
         name: `name`,
-        message: `What is the employee's Name?`
+        message: `Welcome new manager! What is the your Name?`
       },
       {
         type: `input`,
         name: `id`,
-        message: `What is the employee's ID number?`,
+        message: `What is your ID number?`,
       },
       {
           type: `input`,
           name: `email`,
-          message: `What is the employee's email address?`
+          message: `What is your email address?`
       }
-
+      {
+        type: `input`,
+        name: `office`,
+        message: `What is your office number?`
+      }
+.then(
+  expandTeam();
+)
 )};
 
-inquirer.prompt(
-  createTeam()
-).then(function(response){
-  let manager = new Manager('dave');
-  inquirer.prompt({
-   message: 'Do you want to add an employee at this time?',
-   name:'continue',
-   type:'confirm'
-    }).then
-})
 
-createTeam()
-// .then(teamData => {
-//   return generateSite/pageTemplate(teamData);
-// })
+// Asks if the manager would like to add more team members, then allows that or creates the page
+function expandTeam() {
+  inquirer.prompt({
+    message: 'Do you want to add an employee at this time?',
+    name:'continue',
+    type:'confirm'
+     }).then(
+       if(answer.continue) {
+         addEmployee();
+       }
+       else {
+         createPage();
+       }
+     )
+};
+
+
+// These three functions ask the questions unique to the different types of employees after the addEmployee function is done
+function addEngineer() {
+  inquirer.prompt(
+    {
+      type: `input`,
+      name: `github`,
+      message: `What is the engineer's github username?`
+    }
+  ).then(
+    expandTeam();
+  )
+};
+
+function addIntern() {
+  inquirer.prompt(
+    {
+      type: `input`,
+      name: `school`,
+      message: `What is the name of the intern's school?`
+    }
+  ).then(
+    expandTeam();
+  )
+};
+
+function addManager() {
+  inquirer.prompt(
+    {
+      type: `input`,
+      name: `office`,
+      message: `What is the manager's office number?`
+    }
+  ).then(
+    expandTeam();
+  )
+};
+
+
+// Adds a new employee if the expandTeam function calls it
+function addEmployee() {
+  return inquirer.prompt(
+    {
+      type: `input`,
+        name: `name`,
+        message: `What is the the employee's Name?`
+    },
+    {
+      type: `input`,
+      name: `id`,
+      message: `What is the employee's ID number?`,
+    },
+    {
+      type: `input`,
+      name: `email`,
+      message: `What is the employee's email address?`
+  },
+    {
+      type: `list`,
+      name: `title`,
+      message: `What is the job title of the employee?`,
+      choices: ['Engineer', 'Intern', 'Manager']
+    },)
+    .then(
+      if(answer.title == "Engineer") {
+        addEngineer();
+      }
+      else if(answer.title = "Intern") {
+        addIntern();
+      }
+      else if(answer.title = "Manager") {
+        addManager();
+      }
+      else {
+        console.log("Please choose a valid option");
+        // reprompt this area in this case
+      }
+    )
+  };
+
+
+// Function call that starts the team creation process
+createTeam();
+
+
+// Stuff to make the page...need to put in a function called createPage
 .then(pageHTML => {
   return writeFile(pageHTML);
 })
@@ -49,5 +146,3 @@ createTeam()
 .catch(err => {
   console.log(err);
 });
-
-// LOTS of questions about how this connects to other files including the generate site, page temple, everything in the lib folder, and the tests. So that is basically everything I guess
